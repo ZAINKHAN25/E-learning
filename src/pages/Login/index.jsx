dc42c43950a86847205f3f1eef98cb2f00302c9e
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2023-10-29 01:27:42 -0700

    Ui me thora aur kam kya

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -6,7 +6,7 @@ function App() {
     return (
         <div className='signUpPage'>
             <div className="signupcard">
-                <h3 className='h3ofsignup'>Logged In To Your Account</h3>
+                <h3 className='h3ofsignup'>Log In</h3>
                 <p className='paraofsignup'>Logged-In to your account or else you can't go to home page or other page</p>
                 <input placeholder='Email Address' type="email" id="signupemail" />
                 <input placeholder='Password' type="password" id="signuppassword" />
