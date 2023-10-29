19314a273ad7a7ade44dc9a9889bfffea66bde57
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2023-10-29 04:22:49 -0700

    kam chal rha hai

--- src/components/All-Posts/index.jsx
+++ src/components/All-Posts/index.jsx
@@ -1,62 +1,67 @@
 import './app.css'
-
 import Allcourses from '../../DummyData/Course.js';
 import { useEffect, useState } from 'react';
 
 function App() {
     let [allcoursesArr, setallcoursesArr] = useState([]);
-    let [currentimg, setcurrentimg] = useState(0)
 
     useEffect(() => {
         setallcoursesArr(Allcourses)
     }, [])
 
-    function previoscourseimg() {
-        if (currentimg === 0) {
-            setcurrentimg(-200)
-        } else {
-            setcurrentimg(currentimg + 100)
-        }
+    function previoscourseimg(index) {
+        // Update the 'currentimg' for the specific course item
+        setallcoursesArr(prevCourses => {
+            const updatedCourses = [...prevCourses];
+            if (updatedCourses[index].currentimg === 0) {
+                updatedCourses[index].currentimg = -200;
+            } else {
+                updatedCourses[index].currentimg += 100;
+            }
+            return updatedCourses;
+        });
     }
 
-    function nextcourseimg() {
-        if (currentimg === -200) {
-            setcurrentimg(0)
-        } else {
-            setcurrentimg(currentimg - 100)
-        }
+    function nextcourseimg(index) {
+        // Update the 'currentimg' for the specific course item
+        setallcoursesArr(prevCourses => {
+            const updatedCourses = [...prevCourses];
+            if (updatedCourses[index].currentimg === -200) {
+                updatedCourses[index].currentimg = 0;
+            } else {
+                updatedCourses[index].currentimg -= 100;
+            }
+            return updatedCourses;
+        });
     }
 
     return (
         <>
-            {
-                allcoursesArr.map((single, i) => {
-                    return (
-                        <div key={i} className='singlediv'>
-                            <div className='overflow-hidden'>
-
-                                <div className="imgarea">
-                                    <img style={{ transform: `translateX(${currentimg}%)` }} className='singleimgofcourse' src={single.imageone} />
-                                    <img style={{ transform: `translateX(${currentimg}%)` }} className='singleimgofcourse' src={single.imagetwo} />
-                                    <img style={{ transform: `translateX(${currentimg}%)` }} className='singleimgofcourse' src={single.imagethree} />
-                                </div>
-                            </div>
-                            <h5 className='headingofcourse'>{single.heading}</h5>
-                            <div className="descritionarea">
-                                {single.description.slice(0,100)}...
-                            </div>
-                            <div className="anlysiscoursediv">
-                                <span>{single.like} likes</span>
-                                <span>{single.veiws} Veiws</span>
-                            </div>
-                            <div className='leftRightBtn'>
-                                <span onClick={previoscourseimg}>{true && '<'}</span>
-                                <span onClick={nextcourseimg}>{true && '>'}</span>
+            {allcoursesArr.map((single, i) => {
+                return (
+                    <div key={i} className='singlediv'>
+                        <div className='overflow-hidden'>
+                            <div className="imgarea">
+                                <img style={{ transform: `translateX(${single.currentimg}%)` }} className='singleimgofcourse' src={single.imageone} />
+                                <img style={{ transform: `translateX(${single.currentimg}%)` }} className='singleimgofcourse' src={single.imagetwo} />
+                                <img style={{ transform: `translateX(${single.currentimg}%)` }} className='singleimgofcourse' src={single.imagethree} />
                             </div>
                         </div>
-                    )
-                })
-            }
+                        <h5 className='headingofcourse'>{single.heading}</h5>
+                        <div className="descritionarea">
+                            {single.description.slice(0, 100)}...
+                        </div>
+                        <div className="anlysiscoursediv">
+                            <span>{single.like} likes</span>
+                            <span>{single.veiws} Veiws</span>
+                        </div>
+                        <div className='leftRightBtn'>
+                            <span onClick={() => nextcourseimg(i)}>{'<'}</span>
+                            <span onClick={() => previoscourseimg(i)}>{'>'}</span>
+                        </div>
+                    </div>
+                )
+            })}
         </>
     )
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,12 +2,14 @@ import './app.css'
 import Navbar from '../../components/Navbar/index.jsx';
 import Allposts from '../../components/All-Posts/index.jsx'
 
-function App(){
+function App() {
     return (
         <div className='homepage'>
             <div className="homewrapper">
-                <Navbar/>
-                <Allposts/>
+                <Navbar />
+                <div className='d-flex flex-wrap justify-content-center'>
+                    <Allposts />
+                </div>
             </div>
         </div>
     )
