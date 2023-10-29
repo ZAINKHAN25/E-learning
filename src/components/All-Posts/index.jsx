18722a855b8f24056f4acfa4f3bec15c5957b65c
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2023-10-29 04:12:15 -0700

    All courses wala kam hogya hai

--- src/components/All-Posts/index.jsx
+++ src/components/All-Posts/index.jsx
@@ -11,18 +11,18 @@ function App() {
         setallcoursesArr(Allcourses)
     }, [])
 
-    function previoscourseimg(){
-        if(currentimg === 0){
+    function previoscourseimg() {
+        if (currentimg === 0) {
             setcurrentimg(-200)
-        } else{
+        } else {
             setcurrentimg(currentimg + 100)
         }
     }
 
-    function nextcourseimg(){
-        if(currentimg === -200){
+    function nextcourseimg() {
+        if (currentimg === -200) {
             setcurrentimg(0)
-        } else{
+        } else {
             setcurrentimg(currentimg - 100)
         }
     }
@@ -32,22 +32,28 @@ function App() {
             {
                 allcoursesArr.map((single, i) => {
                     return (
-                    <div key={i} className='singlediv'>
-                        <div  className="imgarea">
-                            <img style={{transform: `translateX(${currentimg}%)`}} className='singleimgofcourse' src={single.imageone} />
-                            <img style={{transform: `translateX(${currentimg}%)`}} className='singleimgofcourse' src={single.imagetwo} />
-                            <img style={{transform: `translateX(${currentimg}%)`}} className='singleimgofcourse' src={single.imagethree} />
+                        <div key={i} className='singlediv'>
+                            <div className='overflow-hidden'>
+
+                                <div className="imgarea">
+                                    <img style={{ transform: `translateX(${currentimg}%)` }} className='singleimgofcourse' src={single.imageone} />
+                                    <img style={{ transform: `translateX(${currentimg}%)` }} className='singleimgofcourse' src={single.imagetwo} />
+                                    <img style={{ transform: `translateX(${currentimg}%)` }} className='singleimgofcourse' src={single.imagethree} />
+                                </div>
+                            </div>
+                            <h5 className='headingofcourse'>{single.heading}</h5>
+                            <div className="descritionarea">
+                                {single.description.slice(0,100)}...
+                            </div>
+                            <div className="anlysiscoursediv">
+                                <span>{single.like} likes</span>
+                                <span>{single.veiws} Veiws</span>
+                            </div>
+                            <div className='leftRightBtn'>
+                                <span onClick={previoscourseimg}>{true && '<'}</span>
+                                <span onClick={nextcourseimg}>{true && '>'}</span>
+                            </div>
                         </div>
-                        <h5 className='headingofcourse'>{single.heading}</h5>
-                        <div className="anlysiscoursediv">
-                            <span>{single.like}</span>
-                            <span>{single.veiws}</span>
-                        </div>
-                        <div className='leftRightBtn'>
-                            <span onClick={previoscourseimg}>{true && '<'}</span>
-                            <span onClick={nextcourseimg}>{true && '>'}</span>
-                        </div>
-                    </div>
                     )
                 })
             }
