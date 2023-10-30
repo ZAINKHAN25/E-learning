9069fe6f07ee67bfea916d55a393885269ff5db0
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2023-10-30 10:40:22 -0700

    Chat Ui Is Done

--- src/pages/Coursemessage/index.jsx
+++ src/pages/Coursemessage/index.jsx
@@ -1,10 +1,39 @@
 import './app.css'
+import DummyData from '../../DummyData/Course.js'
+import Navbar from '../../components/Navbar/index.jsx'
 
-function App(){
+function App() {
     return (
-        <>
-            hello
-        </>
+        <div className='chatpage'>
+            <div className='position-fixed w-100 top-0'>
+                <Navbar />
+            </div>
+            <div className="chatwrapper">
+                <div className="chatcommunitydiv position-fixed">
+                    <i class="fa-solid fa-less-than fs-2 backToHomeIcon" title='Go back to Home'></i>
+                    <img className='chatcommunityimg' src={DummyData[0].imageone} alt="" />
+                    <h3 className='fs-1'>Zain Chat</h3>
+                </div>
+                <div className="mainchatarea">
+                    <div className="mineChat">Hey Friend How Are you <span className='dateofchat'>5 hour's ago</span></div>
+                    <div className="friendChat">I am fine <span className='dateofchat'>3 hour's ago</span></div>
+                    <div className="mineChat">Hey Friend How Are you <span className='dateofchat'>5 hour's ago</span></div>
+                    <div className="mineChat">Hey Friend How Are you <span className='dateofchat'>5 hour's ago</span></div>
+                    <div className="friendChat">I am fine <span className='dateofchat'>3 hour's ago</span></div>
+                    <div className="friendChat">I am fine <span className='dateofchat'>3 hour's ago</span></div>
+                    <div className="mineChat">Hey Friend How Are you <span className='dateofchat'>5 hour's ago</span></div>
+                    <div className="mineChat">Hey Friend How Are you <span className='dateofchat'>5 hour's ago</span></div>
+                    <div className="friendChat">I am fine <span className='dateofchat'>3 hour's ago</span></div>
+                    <div className="friendChat">I am fine <span className='dateofchat'>3 hour's ago</span></div>
+                    <div className="mineChat">Hey Friend How Are you <span className='dateofchat'>5 hour's ago</span></div>
+                    <div className="mineChat">Hey Friend How Are you <span className='dateofchat'>5 hour's ago</span></div>
+                </div>
+                <div className="inputdiv fs-5">
+                    <input type="text" placeholder='Message...' className="inputOfChat" />
+                    <i class="fa-solid fa-paper-plane"></i>
+                </div>
+            </div>
+        </div>
     )
 }
 
